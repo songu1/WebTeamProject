33849b2f28644d91a9153b3f5c18331982b8bbc8
JMK1007 <coxld010@naver.com>
2021-05-31 15:09:03 +0900

    login기능 수정

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 const expressSession = require('express-session');
@@ -6,7 +5,7 @@ const mongoose = require('mongoose');
 
 const User = require('./models/User.js');
 const Clothes = require('./models/Clothes.js');
-const registerController = require('./controller/register');
+// const registerController = require('./controller/newUser');
 const loginUserController = require('./controller/loginUser');
 const redirectIfAuthenticateMiddleware = require('./middleware/redirectAuthenticatedMiddleware');
 const storeUserController = require('./controller/storeUser');
@@ -14,23 +13,20 @@ const loginController = require('./controller/login');
 const logoutController = require('./controller/logout');
 mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 
-
-const newUserController = require("./controller/newUser");
-
+const newUserController = require('./controller/newUser');
 
 const fileUpload = require('express-fileupload');
-const validateMiddleware = require("./middleware/validateMiddleware");
+const validateMiddleware = require('./middleware/validateMiddleware');
 const ootdController = require('./controller/ootd');
 const storePostController = require('./controller/storePost');
 
-
 const app = new express();
-const ejs = require("ejs");
-const loginUser = require("./controller/loginUser");
-app.set("view engine", "ejs");
+const ejs = require('ejs');
+const loginUser = require('./controller/loginUser');
+app.set('view engine', 'ejs');
 app.use(fileUpload());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
@@ -45,26 +41,22 @@ app.use('*', (req, res, next) => {
   next();
 });
 
-app.use('/posts/store',validateMiddleware);
+app.use('/posts/store', validateMiddleware);
 
 app.listen(4000, () => {
   console.log(`App listeing on port 4000`);
 });
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   const clothes = await Clothes.find({});
-  res.render("index", {
+  res.render('index', {
     clothes,
   });
 });
 
-<<<<<<< HEAD
 app.get('/login', redirectIfAuthenticateMiddleware, loginController);
 
-app.get('/ootd', (req, res) => {
-  res.render('ootd');
-});
-app.get('/register', redirectIfAuthenticateMiddleware, registerController);
+app.get('/register', redirectIfAuthenticateMiddleware, newUserController);
 app.get('/write', (req, res) => {
   res.render('write');
 });
@@ -78,26 +70,7 @@ app.post(
 app.post('/user/login', redirectIfAuthenticateMiddleware, loginUserController);
 
 app.get('/logout', logoutController);
-=======
-app.get("/login", (req, res) => {
-  res.render("login");
-});
 
-app.get('/ootd',ootdController);
+app.get('/ootd', ootdController);
 
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-app.get("/write", (req, res) => {
-  res.render("write");
-});
-
-app.post('/posts/store',storePostController);
-
-app.post("/register/newUser", async (req, res) => {
-  await User.create(req.body);
-  res.redirect("/");
-});
-
-app.post("/user/login", loginUserController);
->>>>>>> 9b12e552186367ae8d2429daeaab0b5831855f69
+app.post('/posts/store', storePostController);
